e76fd5fa6849159e914e6321a9de3b3fed694ff5
Kyle Grenell <kyle.grenell@gmail.com>
2017-02-06 13:46:11 +0000

    stae now updates with selectedVideo

--- src/index.js
+++ src/index.js
@@ -3,24 +3,31 @@ import ReactDOM from 'react-dom'
 import YTSearch from 'youtube-api-search'
 import SearchBar from './components/search_bar'
 import VideoList from './components/video_list'
+import VideoDetail from './components/video_detail'
 const API_KEY = 'AIzaSyDetN9Q18C5GAdvLPy6PElFRpzYGYItnb4'
 
 class App extends Component {
   constructor(props){
     super(props)
 
-    this.state = { videos: [] }
+    this.state = { 
+      videos: [],
+      selectedVideo: null 
+    };
 
-    YTSearch({key: API_KEY, term: 'surfboards'}, (videos) => {
-      this.setState({videos})
-      // console.log(videos)
+    YTSearch({key: API_KEY, term: 'synthesizer'}, (videos) => {
+      this.setState({
+        videos: videos,
+        selectedVideo: videos[0] 
       })
+    })
   }
 
   render(){
     return (
       <div>
       <SearchBar />
+      <VideoDetail video={this.state.selectedVideo}/>
       <VideoList videos={this.state.videos} />      
       </div>
       )
