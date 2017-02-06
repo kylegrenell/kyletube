bf5059c30e017de5d43ad9d5297c9d5b2438da2f
Kyle Grenell <kyle.grenell@gmail.com>
2017-02-06 01:19:48 +0000

    array of objects in list

--- src/index.js
+++ src/index.js
@@ -1,18 +1,32 @@
-import React from 'react';
+import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
-
+import YTSearch from 'youtube-api-search';
 import SearchBar from './components/search_bar';
-
+import VideoList from './components/video_list';
 const API_KEY = 'AIzaSyDetN9Q18C5GAdvLPy6PElFRpzYGYItnb4';
 
 // functional based component - used when taking in some inofrmation and spitting out some jsx.
 // can contain a class based component
-const App = () => {
-  return (
-  <div>
-  <SearchBar />
-  </div>
-  );
+class App extends Component {
+  constructor(props){
+    super(props);
+
+    this.state = { videos: [] };
+
+    YTSearch({key: API_KEY, term: 'surfboards'}, (videos) => {
+      this.setState = ({videos});
+      // es6 when key and value are same name you can use one name, it gets rendered to videos: videos. Cleaner code.
+      });
+  }
+
+  render(){
+    return (
+      <div>
+      <SearchBar />
+      <VideoList videos={this.state.videos} />      
+      </div>
+      );
+  }
 }
 
 ReactDOM.render(<App />, document.querySelector('.container'));
