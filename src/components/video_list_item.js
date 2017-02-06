c553c9fded154ce73417f52319e955459b3494f5
Kyle Grenell <kyle.grenell@gmail.com>
2017-02-06 12:34:17 +0000

    video list displaying with api video thumbnails and decription

--- src/components/video_list_item.js
+++ src/components/video_list_item.js
@@ -1,7 +1,23 @@
 import React from 'react';
+// passing in props and then decalring const video = props.video; -- is identical to ({video}) being passed as the argument in es6. it has the property of video, it grabs that video and decalresa new variable called video
+const VideoListItem = ({video}) => {
+  // console.log(video);
 
-const VideoListItem = (props) => {
-  return <li>Video</li>
+  const imageUrl = video.snippet.thumbnails.default.url;
+  //response from the youtube api
+
+  return (
+    <li className="list-group-item">
+      <div className="video-list media">
+        <div className="media-left">
+          <img className="media-object" src={imageUrl} />
+        </div>
+        <div className="media-body">
+          <div className="media-heading">{video.snippet.title}</div>
+        </div>
+      </div>
+    </li>
+    );
 };
 
 export default VideoListItem;
