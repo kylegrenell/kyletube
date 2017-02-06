1849e1ed63768b817d831df18a189d154e984289
Kyle Grenell <kyle.grenell@gmail.com>
2017-02-06 11:50:28 +0000

    youtube api etag key added to returned list items

--- src/components/video_list.js
+++ src/components/video_list.js
@@ -1,17 +1,17 @@
-import React from 'react'
-import VideoListItem from './video_list_item'
+import React from 'react';
+import VideoListItem from './video_list_item';
 
 const VideoList = (props) => {
   const videoItems = props.videos.map( (video) => {
-    return 
-    <VideoListItem video={video} />
-  })
+    return <VideoListItem key={video.etag} video={video} />
+    // etag is in chrome dev tools under network -> search giving each item a unique key
+  });
 
-  return(
+  return (
     <ul className="col-md-4 list-group">
     {videoItems}
     </ul>
     )
-}
+};
 
-export default VideoList
+export default VideoList;
